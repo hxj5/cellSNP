e27654f3c0a94a47ebf6089ad0322f14d39dae07
hxj5 <hxj5@hku.hk>
2020-09-23 09:16:46 +0800

    generalize gene to region, add bed file and fixed-size support

--- cellSNP/cellFeatureCount.py
+++ cellSNP/cellFeatureCount.py
@@ -14,28 +14,28 @@ from optparse import OptionParser, OptionGroup
 
 from .version import __version__
 from .utils.gtf_utils import load_genes
-from .utils.count_utils import feature_count
+from .utils.count_utils import feature_count, bed2reg, chr2reg
 
 FID = None
 PROCESSED = 0
-TOTAL_GENE = 0
+TOTAL_REGION = 0
 START_TIME = time.time()
 
 def show_progress(RV=None):
-    global PROCESSED, TOTAL_GENE, START_TIME, FID
+    global PROCESSED, TOTAL_REGION, START_TIME, FID
     if RV is not None:
         FID.writelines(RV)
     
     PROCESSED += 1
     bar_len = 20
     run_time = time.time() - START_TIME
-    percents = 100.0 * PROCESSED / TOTAL_GENE
+    percents = 100.0 * PROCESSED / TOTAL_REGION
     filled_len = int(round(bar_len * percents / 100))
     bar = '=' * filled_len + '-' * (bar_len - filled_len)
     
-    sys.stdout.write('\r[cf-count] [%s] %.1f%% done in %.1f sec.' 
+    sys.stderr.write('[cf-count] [%s] %.1f%% done in %.1f sec.\n' 
         % (bar, percents, run_time))
-    sys.stdout.flush()
+    sys.stderr.flush()
     return RV
 
 def main():
@@ -46,20 +46,20 @@ def main():
     parser = OptionParser()
     parser.add_option("--samFile", "-s", dest="sam_file", default=None,
         help=("Indexed & sorted sam/bam/cram file, possibly from CellRanger."))
-    parser.add_option("--outDir", "-o", dest="out_dir", default=None,
+    parser.add_option("--outDir", "-O", dest="out_dir", default=None,
         help=("Full path for output direcotry [default: $samFile_dir]"))
     parser.add_option("--barcodeFile", "-b", dest="barcode_file", default=None,
         help=("A plain file listing all effective cell barcodes."))
-    parser.add_option("--GFF", "-G", dest="GFF_file", default=None,
-        help=("A GFF/GTF file containing features annotation"))
+    parser.add_option("--regFile", "-r", dest="reg_file", default=None,
+        help=("Path to region file."))
+    parser.add_option("--regType", "-T", dest="reg_type", default=None,
+        help=("Region type, one of bed|gff."))
+    parser.add_option("--binSize", "-B", type="int", dest="bin_size", default=50,
+        help=("Fixed size of bin in kb. it will be used when @p regFile or @p regType is not set [default: %default]"))
     
     group1 = OptionGroup(parser, "Optional arguments")
     group1.add_option("--nproc", "-p", type="int", dest="nproc", default=1,
         help="Number of subprocesses [default: %default]")
-    group1.add_option("--featureType", "-t", dest="feature_type", default="gene",
-        help=("Feature type to culculate [default: %default]."))
-    # group1.add_option("--IDattribute", "-i", dest="id_attribute", default="ID",
-    #     help=("Id tag for feature in GFF file [default: %default]."))
     group1.add_option("--cellTAG", dest="cell_tag", default="CB", 
         help="Tag for cell barcodes, turn off with None [default: %default]")
     group1.add_option("--UMItag", dest="UMI_tag", default="None", 
@@ -77,6 +77,7 @@ def main():
     parser.add_option_group(group1)
     parser.add_option_group(group2)
 
+    # check options and args
     (options, args) = parser.parse_args()
     if len(sys.argv[1:]) == 0:
         print("Welcome to cf-count in cellSNP v%s!\n" %(__version__))
@@ -101,48 +102,85 @@ def main():
         out_dir = options.out_dir
     if os.path.isdir(os.path.abspath(out_dir)) == False:
         print("Error: No such directory for file\n -- %s" %out_dir)
-        sys.exit(1)        
+        sys.exit(1)   
+
+    reg_file, reg_type = options.reg_file, options.reg_type
+    if not reg_file or not os.path.isfile(reg_file) or not reg_type or reg_type.lower() not in ("bed", "gff"):
+        reg_file = None
+        reg_type = None
+    else:
+        options.bin_size = None
+        reg_type = reg_type.lower()
+
+    if reg_file is None and (not options.bin_size or options.bin_size <= 0):
+        print("Error: either region file or a positive bin_size should be provided!")
+        sys.exit(1)    
     
     nproc = options.nproc
     min_LEN = options.min_LEN
     min_MAPQ = options.min_MAPQ
     max_FLAG = options.max_FLAG
     
-    # load features from GTF file
-    genes = load_genes(options.GFF_file, tranTag="", exonTag="")
-    gene_ids = [g.geneID for g in genes]
-    if len(np.unique(gene_ids)) != len(gene_ids):
-        gene_ids = ["gene%d" %(x + 1) for x in range(len(genes))]
+    # load regions
+    reg_list = None
+    if reg_file is not None:
+        if reg_type == "bed":
+            reg_list = bed2reg(reg_file)
+        elif reg_type == "gff":
+            reg_list = load_genes(reg_file, tranTag="", exonTag="")
+        else:   # should not come here!
+            print("Error: wrong region type!")
+            sys.exit(1)
+        if not reg_list:
+            print("Error: empty region file or failing to parse region file!")
+            sys.exit(1)
+    else:  # fixed-size regions/bins
+        sam_fp = pysam.AlignmentFile(options.sam_file, "r")   # file format auto-detected.
+        reg_list = []
+        target_chroms = [str(i) for i in range(1, 23)] + ["X", "Y"]
+        for chrom in target_chroms:
+            _regions = chr2reg(chrom, sam_fp, options.bin_size * 1000)
+            if _regions is None:
+                print("Error: cannot split chromsome %s to bins!" % chrom)
+                sam_fp.close()
+                sys.exit(1)
+            reg_list.extend(_regions)
+        sam_fp.close()
 
-    # save cell barcodes and genes
+    reg_ids = [reg.id for reg in reg_list]
+    feature_name = "gene" if reg_type == "gff" else "region"
+    if len(np.unique(reg_ids)) != len(reg_ids):
+        reg_ids = ["%s%d" % (feature_name, x + 1) for x in range(len(reg_list))]
+
+    # save cell barcodes and regions
     fid = open(out_dir + "/features.tsv", "w")
-    for _gene_id in gene_ids:
-        fid.writelines(_gene_id + "\n")
+    for _reg_id in reg_ids:
+        fid.writelines(_reg_id + "\n")
     fid.close()
     print("[cf-count] Count reads for %d features in %d cells with %d cores." 
-          %(len(gene_ids), len(barcodes), nproc))
+          % (len(reg_ids), len(barcodes), nproc))
 
     fid = open(out_dir + "/barcodes.tsv", "w")
     for _cell_id in barcodes:
         fid.writelines(_cell_id + "\n")
     fid.close()
 
-    global FID, TOTAL_GENE
-    TOTAL_GENE = len(gene_ids)
+    global FID, TOTAL_REGION
+    TOTAL_REGION = len(reg_ids)
     FID = open(out_dir + "/matrix.mtx", "w")
 
     if nproc > 1:
         result = []
         pool = multiprocessing.Pool(processes=nproc)
-        for ii in range(len(genes)):
+        for ii in range(len(reg_list)):
             result.append(pool.apply_async(feature_count, (sam_file, 
-                barcodes, genes[ii], ii, cell_tag, UMI_tag, min_MAPQ, max_FLAG, min_LEN), 
+                barcodes, reg_list[ii], ii, cell_tag, UMI_tag, min_MAPQ, max_FLAG, min_LEN), 
                 callback=show_progress))
         pool.close()
         pool.join()
     else:
-        for ii in range(len(genes)):
-            RV = feature_count(sam_file, barcodes, genes[ii], ii, cell_tag, 
+        for ii in range(len(reg_list)):
+            RV = feature_count(sam_file, barcodes, reg_list[ii], ii, cell_tag, 
                                UMI_tag, min_MAPQ, max_FLAG, min_LEN)
             show_progress(RV)
 
@@ -152,11 +190,11 @@ def main():
     fid = open(out_dir + "/matrix.mtx", "w")
     fid.writelines(
         "%" + "%MatrixMarket matrix coordinate integer general\n" + "%\n")
-    fid.writelines("%d\t%d\t%d\n" %(len(genes), len(barcodes), data.shape[0]))
+    fid.writelines("%d\t%d\t%d\n" % (len(reg_list), len(barcodes), data.shape[0]))
     
     sort_idx = np.argsort(data[:, 0] * len(barcodes) + data[:, 1])
     for ii in sort_idx:
-        fid.writelines("%d\t%d\t%d\n" %(data[ii, 0], data[ii, 1], data[ii, 2]))
+        fid.writelines("%d\t%d\t%d\n" %(data[ii, 0] + 1, data[ii, 1] + 1, data[ii, 2]))  # set cell_idx and reg_idx to 1-based
     fid.close()
     
     print("")
--- cellSNP/utils/count_utils.py
+++ cellSNP/utils/count_utils.py
@@ -3,7 +3,9 @@
 # Date: 27/04/2020
 
 ## Note, this is a very basic read counting for features
- 
+
+import os
+import gzip 
 import sys
 import pysam
 import numpy as np
@@ -11,34 +13,113 @@ from .base_utils import id_mapping, unique_list
 from .pileup_utils import check_pysam_chrom
 from ..version import __version__
 
-
 global CACHE_CHROM
 global CACHE_SAMFILE
 CACHE_CHROM = None
 CACHE_SAMFILE = None
 
+class Region:
+    '''
+    @abstract      A wrapper for different region formats, e.g. bed/gff
+    @param chrom   Chromosome name [str]
+    @param start   Start pos of this region: 1-based, included [int]
+    @param stop    End pos of this region: 1-based, included [int]
+    @param _id     Region ID [str]
+    '''
+    def __init__(self, chrom=None, start=None, stop=None, _id=None):
+        self.chrom = chrom
+        self.start = start
+        self.stop = stop
+        self.id = _id
+    
+def bed2reg(bed_file):
+    """
+    @abstract        Get list of regions (1-based) from bed file
+    @param bed_file  Path to bed file [str]
+    @return          A list of Region objects if success, None otherwise [list]
+    """
+    if not bed_file or not os.path.isfile(bed_file):
+        return None
 
-def fetch_reads(sam_file, gene, cell_tag="CR", UMI_tag="UR", min_MAPQ=20, 
+    lines = None
+    if os.path.splitext(bed_file)[1] in (".gz", ".gzip"):
+        with gzip.open(bed_file, "rt") as zfp:
+            lines = zfp.readlines()
+    else:
+        with open(bed_file, "r") as fp:
+            lines = fp.readlines()
+    
+    reg_list = []
+    for idx, ln in enumerate(lines):   # it's probably fine to use enumerate here for bed files are usually small.
+        parts = ln[:-1].split("\t")
+        try:
+            start, stop = int(parts[1]), int(parts[2])
+            _id = "%s:%d-%d" % (parts[0], start + 1, stop)
+        except (IndexError, ValueError) as e:
+            print("Error: invalid bed record in No.%d line: %s" % (idx + 1, str(e)))
+            return None
+        reg_list.append(Region(parts[0], start + 1, stop, _id))
+
+    return reg_list
+
+def chr2reg(chrom, sam_fp, bin_size):
+    """
+    @abstract        Split the chromosome to several fixed-size bins.
+    @param chrom     Chromosome name [str]
+    @param sam_fp    The AlignmentFile object whose header will be used to extract chrom length [pysam.AlignmentFile]
+    @param bin_size  Size of the bin, an integer [int]
+    @return          A list of Region objects if success, None otherwise [list]
+    """
+    if chrom not in sam_fp.references:
+        chrom = chrom[3:] if chrom.startswith("chr") else "chr" + chrom
+        if chrom not in sam_fp.references:
+            return None
+
+    total_len = sam_fp.get_reference_length(chrom)
+    bin_size = int(bin_size)
+    if not total_len or total_len <= 0 or bin_size <= 0: 
+        return None
+    nbins = total_len // bin_size
+    if total_len % bin_size != 0: nbins += 1
+
+    reg_list = []
+    for i in range(nbins):
+        start = bin_size * i + 1
+        stop = bin_size * (i + 1)
+        _id = "%s:%d-%d" % (chrom, start, stop)
+        reg_list.append(Region(chrom, start, stop, _id))
+    
+    return reg_list
+
+def gene2reg(genes):
+    """
+    @abstract     Create regions from gff genes
+    @param genes  A list/set/tuple of @class Genes [list/set/tuple]
+    @return       A list of Region objects if success, None otherwise [list]
+    """
+    return [Region(g.chrom, g.start, g.stop, g.geneID) for g in genes]
+
+def fetch_reads(sam_file, region, cell_tag="CR", UMI_tag="UR", min_MAPQ=20, 
                 max_FLAG=255, min_LEN=30):
-    """ Fetch all reads mapped to a given gene.
+    """ Fetch all reads mapped to a given region.
     Filtering is also applied, including cell and UMI tags and read mapping 
     quality.
     """
-    if sam_file is None or gene is None:
+    if sam_file is None or region is None:
         if sam_file is None:
             print("Warning: samFile is None")
-        if gene is None:
-            print("Warning: gene is None")
+        if region is None:
+            print("Warning: region is None")
         return np.array([]), np.array([])
 
-    samFile, _chrom = check_pysam_chrom(sam_file, gene.chrom)
+    samFile, _chrom = check_pysam_chrom(sam_file, region.chrom)
     
     UMIs_list, cell_list = [], []
-    for _read in samFile.fetch(_chrom, gene.start - 1, gene.stop):
+    for _read in samFile.fetch(_chrom, region.start - 1, region.stop):
         ## filtering reads
         # this might be further speed up
-        overhang = sum((np.array(_read.positions) >= gene.start) * 
-                       (np.array(_read.positions) <= gene.stop))
+        overhang = sum((np.array(_read.positions) >= (region.start - 1)) *   
+                       (np.array(_read.positions) <= (region.stop - 1)))
 
         if _read.mapq < min_MAPQ or _read.flag > max_FLAG or overhang < min_LEN: 
             continue
@@ -62,12 +143,11 @@ def fetch_reads(sam_file, gene, cell_tag="CR", UMI_tag="UR", min_MAPQ=20,
 
     return cell_list_uniq, read_count
 
-
-def feature_count(sam_file, barcodes, gene, g_index, cell_tag, UMI_tag, 
+def feature_count(sam_file, barcodes, region, reg_index, cell_tag, UMI_tag, 
     min_MAPQ, max_FLAG, min_LEN):
     """Fetch read count for a given feature.
     """
-    cell_list_uniq, read_count = fetch_reads(sam_file, gene, cell_tag, UMI_tag, 
+    cell_list_uniq, read_count = fetch_reads(sam_file, region, cell_tag, UMI_tag, 
         min_MAPQ, max_FLAG, min_LEN)
 
     if len(cell_list_uniq) > 0:
@@ -80,7 +160,7 @@ def feature_count(sam_file, barcodes, gene, g_index, cell_tag, UMI_tag,
         
         out_list = []
         for j in range(len(idx2)):
-            out_list.append("%d\t%d\t%d" %(g_index, idx2[j], read_count[idx1[j]]))
+            out_list.append("%d\t%d\t%d" %(reg_index, idx2[j], read_count[idx1[j]]))
         return "\n".join(out_list) + "\n"
     else:
         return None
